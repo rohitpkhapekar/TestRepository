95d7a192fd3f6632df261d51fa89527bcb4177c9
Khapekar <khapekro@vertafore.com>
2018-09-18 22:34:04 -0600

    node module reinstalled

---
+++ src/components/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export class App extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+
+
+    render(){
+        return <div className="container">
+            Hello World
+        </div>
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path')
+var HTMLPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: {
+        main: './src/main.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 8888,
+        historyApiFallback: true,
+    },
+    mode: 'development',
+    resolve: {
+        extensions: ['.jsx', '.js']
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['react', 'env', 'stage-0'],
+                    plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+
+                }
+            }
+        }]
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HTMLPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
